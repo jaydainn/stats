a1ae76f951b4a006d1680e31a5779fa947cd87a4
jaydainn <37037667+jaydainn@users.noreply.github.com>
2021-06-07 11:35:51 +0200

    🚀 ex1

--- TP2-1.py
+++ TP2-1.py
@@ -11,12 +11,21 @@ p = 0.3
 plt.figure()
 plt.title("Loi Binomiale avec numpy")
 plt.hist(np.random.binomial(n , p , size) , bins = 60)
-plt.show()
+
 
 plt.figure()
-plt.title("Loi binomiale avec numpy")
+plt.title("Loi binomiale avec scipy")
+xval = np.arange(0 ,20 ,  1)
+plt.stem(xval , binom.pmf(xval ,  20 , 0.3) , )
+
 
 plt.figure()
 plt.title("Loi Normale avec numpy")
 plt.hist(np.random.normal(n , p , size) , bins=100)
+plt.show()
+
+plt.figure()
+plt.title("Loi Normale avec scipy")
+xval = np.arange(0,20,0.01)
+plt.plot(xval , norm.pdf(xval , 20/2 , 0.3))
 plt.show()
--- TP2-3.py
+++ TP2-3.py
@@ -6,6 +6,7 @@ NB2 = [30,40,50,60,70,80]
 NB1 =[10,20,30,40,50,60]
 NB3 = [40,50,60,70,80,90]
 T=[2,4,6,8,10,12,14,16,18,20]
+T1=[10,12,14,16,18,20]
 Categorie = ["Categorie1", "Categorie2", "Categorie3", "Categorie4"]
 Nombre = [5000, 26000, 21400, 12000]
 
@@ -14,18 +15,18 @@ plt.xlabel("Temps")
 plt.ylabel("Pieces Conformes")
 
 fig , axs = plt.subplots(3)
-axs[0].plot(T[:len(NB1)] , NB1 ,  "-r"  )
-axs[0].plot(T[:len(NB2)] , NB2 , "*b"  )
-axs[0].plot(T[:len(NB3)] , NB3 ,"+g" )
+axs[0].plot(T1[:len(NB1)] , NB1 ,  "-r"  )
+axs[0].plot(T1[:len(NB2)] , NB2 , "*b"  )
+axs[0].plot(T1[:len(NB3)] , NB3 ,"+g" )
 
 
 axs[0].set_xlabel("Temps")
 axs[0].set_ylabel("Pieces Conformes")
 
 
-axs[1].plot(T[:len(NB1)] , NB1 ,  "r" , linewidth=7  )
-axs[1].plot(T[:len(NB2)] , NB2 , "b" , linewidth = 12  )
-axs[1].plot(T[:len(NB3)] , NB3 ,"g"  , linewidth=10)
+axs[1].plot(T1[:len(NB1)] , NB1 ,  "r" , linewidth=7  )
+axs[1].plot(T1[:len(NB2)] , NB2 , "b" , linewidth = 12  )
+axs[1].plot(T1[:len(NB3)] , NB3 ,"g"  , linewidth=10)
 
 
 axs[1].set_xlabel("Temps")
